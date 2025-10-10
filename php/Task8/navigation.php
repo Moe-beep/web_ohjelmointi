426c68fec40f38b9c1694c20d687a663daceffa1
Mint <moe.naing@vuoksi.fi>
2025-10-10 08:42:35 +0300

    done task 8

--- php/Task8/navigation.php
+++ php/Task8/navigation.php
@@ -9,6 +9,17 @@ $pages = [
 // Get the file name of the current page
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
+<head>
+    <!--Font links from google-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">>
+
+    <!--Link for the Font Awsome web page found in cdnj.com which are logos for the portfolio-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css"
+        integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
 <nav>
     <ul>
         <!--Step through the pages with a foreach statement. Set the class="active" to the <li> element of the current page. Create href links in a loop.  -->
@@ -21,11 +32,14 @@ $current_page = basename($_SERVER['PHP_SELF']);
 </nav>
 
 <style>
+    body {
+        font-family: 'Poppins', sans-serif;
+    }
     nav ul {
         list-style-type: none;
-        padding: 0;
+        padding: 20px 0;
         display: flex;
-        gap: 10px;
+        background-color: black;
     }
 
     nav ul li {
@@ -34,9 +48,8 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
     nav ul li a {
         text-decoration: none;
-        color: #007bff;
-        padding: 5px 10px;
-        border-radius: 5px;
+        color: white;
+        padding: 20px 10px;
     }
 
     nav ul li a:hover {
